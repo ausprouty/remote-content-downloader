757a738e398bb33ff54238d8aa2d1e692b0c616b
ausprouty <bob.prouty@powertochange.org.au>
2024-07-26 19:38:53 +1000

    Form looks nice

--- modules/rcd_link_select/rcd_link_select.php
+++ modules/rcd_link_select/rcd_link_select.php
@@ -1,5 +1,12 @@
 <?php
+/*
+Plugin Name: Your Plugin Name
+Description: Your plugin description.
+Version: 1.0
+Author: Your Name
+*/
 
+// Enqueue necessary scripts and styles
 function enqueue_rcd_scripts_and_styles() {
     // Enqueue jQuery if not already included
     wp_enqueue_script('jquery');
@@ -18,7 +25,6 @@ function enqueue_rcd_scripts_and_styles() {
 }
 add_action('wp_enqueue_scripts', 'enqueue_rcd_scripts_and_styles');
 
-
 function rcd_link_select_shortcode($atts) {
     // Shortcode attributes
     $atts = shortcode_atts(array(
@@ -30,7 +36,7 @@ function rcd_link_select_shortcode($atts) {
         return '<p>Invalid file or name attribute.</p>';
     }
     
-  // Output the hyperlink
+    // Output the hyperlink
     $output = '<a href="#" class="resource-download-link" data-file="' . esc_attr($atts['file']) . '">' . esc_html($atts['name']) . '</a>';
     $output .= '<div id="resource-download-form" style="display:none;">';
     $output .= '<form id="download-form">';
@@ -38,17 +44,16 @@ function rcd_link_select_shortcode($atts) {
     $output .= '<input type="email" id="email" name="email" placeholder="Email" required>';
     $output .= '<input type="text" id="country" name="country" placeholder="Country" required>';
     $output .= '<input type="text" id="state" name="state" placeholder="State" required>';
-    $output .= '<div class="checkbox-group">';
-    $output .= '<input type="checkbox" id="checkbox1" name="checkbox1"><label for="checkbox1">Checkbox 1</label>';
-    $output .= '<input type="checkbox" id="checkbox2" name="checkbox2"><label for="checkbox2">Checkbox 2</label>';
-    $output .= '<input type="checkbox" id="checkbox3" name="checkbox3"><label for="checkbox3">Checkbox 3</label>';
-    $output .= '</div>';
-    
-    $output .= '<textarea id="comments" name="comments" placeholder="How can we pray for you?"></textarea>';
+    $output .= '<div class="checkbox-group"><input type="checkbox" id="checkbox1" name="checkbox1"><label for="checkbox1">Checkbox 1</label></div>';
+    $output .= '<div class="checkbox-group"><input type="checkbox" id="checkbox2" name="checkbox2"><label for="checkbox2">Checkbox 2</label></div>';
+    $output .= '<div class="checkbox-group"><input type="checkbox" id="checkbox3" name="checkbox3"><label for="checkbox3">Checkbox 3</label></div>';
+    $output .= '<label for="comments">How can we pray for you?</label>';
+    $output .= '<textarea id="comments" name="comments" placeholder="Write your prayer request here..."></textarea>';
     $output .= '<button type="button" id="download-resource-button">Download Resource</button>';
     $output .= '</form>';
     $output .= '</div>';
+    
     return $output;
-    }
+}
 add_shortcode('rcd_link_select', 'rcd_link_select_shortcode');
-
+?>
