b166c740056cfb71fb8219ad0196fab57f6f8ebb
ausprouty2 <bob.prouty@powertochange.org.au>
2024-10-05 11:16:24 +1000

    shortened tract select

--- modules/rcd-tract-select/rcd-tract-select.php
+++ modules/rcd-tract-select/rcd-tract-select.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: RCD Tracts Select
 Description: A form to select and retrieve tract from an external API.
-Version: 1.0
+Version: 1.1
 Author: Your Name
 */
 
@@ -12,19 +12,22 @@ define('RCD_TRACT_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 // Include the form file
 include_once RCD_TRACT_PLUGIN_DIR . 'includes/rcd-render-tract-form.php';
-writeLog("rcd-tract-select-15", 'rcd-tract-select');
-function rcd_tract_enqueue_global_scripts() {
-    // Enqueue CSS
-    wp_enqueue_style('rcd-tract-style', RCD_TRACT_PLUGIN_URL . 'css/rcd-tract.css');
 
-    // Enqueue JavaScript files 
+// Enqueue global scripts and styles
+function rcd_tract_enqueue_global_scripts() {
+    wp_enqueue_style('rcd-tract-style', RCD_TRACT_PLUGIN_URL . 'css/rcd-tract.css');
     wp_enqueue_script('rcd-tract-form-options', RCD_TRACT_PLUGIN_URL . 'js/rcd-tract-form-options.js', array('jquery'), null, true);
-}   
+}
 add_action('wp_enqueue_scripts', 'rcd_tract_enqueue_global_scripts');
-writeLog("rcd-tract-select-24", 'rcd-tract-select');
-// Include shortcode files
-include_once RCD_TRACT_PLUGIN_DIR . 'includes/shortcode-bilingual-book.php';
-include_once RCD_TRACT_PLUGIN_DIR . 'includes/shortcode-bilingual-page.php';
-include_once RCD_TRACT_PLUGIN_DIR . 'includes/shortcode-foreign-bilingual-book.php';
-include_once RCD_TRACT_PLUGIN_DIR . 'includes/shortcode-single-language-book.php';
-include_once RCD_TRACT_PLUGIN_DIR . 'includes/shortcode-single-language-page.php';
+
+// Unified shortcode function
+function rcd_tract_select_form($atts) {
+    // Set default attributes and merge with incoming attributes
+    $atts = shortcode_atts(array(
+        'type' => 'bilingual-page' // default tract type
+    ), $atts, 'rcd_tract_select');
+
+    // Pass the tract type to the common form renderer
+    return rcd_render_tract_form($atts['type']);
+}
+add_shortcode('rcd_tract_select', 'rcd_tract_select_form');
